c23d191c4e8b0fb6abbbc2f0881007b10efb0ea0
siddhesh <siddheshsagar3182001@gmail.com>
2023-02-02 13:58:21 +0530

    commited

---
+++ README.md
@@ -0,0 +1,8 @@
+# Razorpay Test App for Python
+Sample App for Razorpay Python Integration
+
+# Steps for Integration
+1. install dependencies from requirement.txt
+3. make a call to './pay' endpoint 
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import razorpay
+import json
+from flask import Flask, render_template, request
+
+razorpay_client = razorpay.Client(auth=("rzp_test_UTtd75CFIcXAzt", "VBB0no9O5Yrbz44NznO7plQA"))
+app = Flask(__name__)
+
+@app.route('/')
+def app_create():
+    # from the checkout page the when the customer hit the "proceed to pay button", a post request will be called to this 
+    # endpoind along with the data associated with the purchase.
+    
+    # this the the temporary data we have provided for testing purpose
+    data = {
+        "amount" : 60000,
+        "currency" : "INR",
+        "receipt" : "dynamic_receipt",
+        "notes":{
+            "name": "Siddhesh",
+            "Payment_for":"testing"
+        }
+    }
+    order = razorpay_client.order.create(data=data)
+    return render_template('main.html',Order=order)
+
+
+@app.route('/paymentHandler', methods=['POST'])
+def payment_handler():
+    response = request.get_json()
+    ver = razorpay_client.utility.verify_payment_signature(response)
+    return str(ver)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
